dd53c4eb0e9b50c29ff46304e0e996384fc4e3a9
Fateen Najib Indramustika <70865641+Teenjb@users.noreply.github.com>
2022-07-06 10:54:25 +0700

    Init React with Tailwindcss

---
+++ oh-ffui-app/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import './index.css';
+import Home from './Page/Home';
+import Login from './Page/Login';
+
+import React, { useState } from "react";
+function App() {
+    return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+
+    );
+}
+
+export default App;
