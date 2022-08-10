fc191dd6507cb7d8b936a68785f256b3c674a30e
Fateen Najib Indramustika <70865641+Teenjb@users.noreply.github.com>
2022-08-10 16:03:03 +0700

    prevent cart and shop

--- oh-ffui-app/src/App.js
+++ oh-ffui-app/src/App.js
@@ -19,8 +19,8 @@ function App() {
             <Route exact path="/home" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/register" element={<Register />} />
-            <Route exact path="/shop" element={<Shop />} />
-            <Route exact path="/cart" element={<Cart />} /> 
+            <Route exact path="/shop" element={<Coming />} />
+            <Route exact path="/cart" element={<Coming />} /> 
             <Route exact path="/about" element={<About />} />
             <Route exact path="/profile" element={<Profile />} />
             <Route exact path="/tickets" element={<Tickets />} />
