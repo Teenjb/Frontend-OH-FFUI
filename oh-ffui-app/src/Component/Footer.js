8c8e318f8796f2d915fb9e40da370c9247264fb6
Qinthara Andini Hananto <70939623+qintharaandini@users.noreply.github.com>
2022-10-09 06:38:01 +0700

    Tambah sponsor dan medpar

--- oh-ffui-app/src/Component/Footer.js
+++ oh-ffui-app/src/Component/Footer.js
@@ -5,9 +5,12 @@ import tiktok from "../Img/icons8-tiktok-100.svg";
 import twitter from "../Img/icons8-twitter-120.png";
 import email from "../Img/icons8-mail-90.png";
 import sp1 from "../Img/logoprima.png"
+import sp2 from "../Img/logopertamina.JPEG";
 import mp1 from "../Img/intipkuliah.PNG"
 import mp2 from "../Img/InfoVolunteers.png"
 import mp3 from "../Img/logo media event.png"
+import mp4 from "../Img/logo info.event_.png"
+import mp5 from "../Img/informasievent.JPG"
 
 function Footer(){
     return(
@@ -15,16 +18,21 @@ function Footer(){
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5 items-center justify-center">
           <div className="grid items-center justify-center">
             <h4 className="font-serif py-5 text-white text-xl text-center leading-7 md:leading-10">Sponsors</h4>
-            <div className="relative items-center justify-center">
-              <img className="h-24 md:h-40" src={sp1} alt="" />
+            <div className="md:flex md:flex-auto md:my-4 items-center justify-center">
+              <img className="h-24 md:px-2 md:h-32" src={sp1} alt="" />
+              <img className="h-24 md:mt-0 mt-3 md:px-2 md:h-32 ml-8" src={sp2} alt="" />
             </div>
           </div>
           <div className="relative items-center justify-center">
             <h4 className="font-serif py-5 text-white text-xl text-center leading-7 md:leading-10">Media Partners</h4>
-            <div className="flex flex-auto md:py-8 items-center justify-center">
-              <img className="h-10 md:h-20" src={mp2} alt="" />
-              <img className="h-10 md:h-20" src={mp3} alt="" />
-              <img className="h-8 md:h-16" src={mp1} alt="" />
+            <div className="flex flex-auto md:pt-4 items-center justify-center">
+              <img className="h-10 md:h-16" src={mp2} alt="" />
+              <img className="h-10 bg-white mx-1 md:h-16" src={mp3} alt="" />
+              <img className="h-8 md:h-14" src={mp1} alt="" />
+            </div>
+            <div className="flex my-1 flex-auto items-center justify-center">
+              <img className="h-8 md:h-14" src={mp5} alt="" />
+              <img className="h-10 bg-white mx-1 md:h-16" src={mp4} alt="" />
             </div>
           </div>
         </div>
